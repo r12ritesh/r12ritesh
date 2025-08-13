4f8c37781f5c6ab553bdcea81bc06459707029b6
Ritesh Kumar <r12ritesh@gmail.com>
2025-08-13 05:32:30 +0000

    OdataModel and LocaljsonModel

--- projectlearn/webapp/controller/CRUDQ.controller.js
+++ projectlearn/webapp/controller/CRUDQ.controller.js
@@ -5,8 +5,26 @@ sap.ui.define([
 
     return Controller.extend("com.sap.project.learn.projectlearn.controller.CRUDQ", {
         onInit() {
+                        // get odata model and json model
+            // var oModel= this.getModel();                    // this will get the odata model
+            // var custModel=this.getModel("custjsonModel");    // this will get the json model
+
+            // oModel.read("/Customers" , {
+            //     success: function(data){
+            //         custModel.setData(data); 
+
+            //     },
+            //     error: function(){
+
+            //     }
+            // });
+
+            // var ojsonModel= new sap.ui.model.json.JSONModel();
+            // ojsonModel.loadData("model/model.json");         // Adjust path if needed
+            // this.getView().setModel(ojsonModel, "custjsonModel");
+            
         },
-        // Logic to navigate to 2nd view
+        // Logic to navigate to previous view
         onPressBackBtn : function(){
            history.go(-1);
         },
